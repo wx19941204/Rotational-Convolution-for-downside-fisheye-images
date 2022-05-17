b3dde6079d8fc820b3d1d01f0addf3acc6336e96
wx19941204 <56708520+wx19941204@users.noreply.github.com>
2022-05-17 15:30:40 +0800

    Add files via upload

--- eval_on_val.py
+++ eval_on_val.py
@@ -26,11 +26,12 @@ import unet
 
 if __name__ == '__main__':
 
-    batch_size = 16
+    batch_size = 8
 
-    network = network_plus.modeling.deeplabv3_resnet18(num_classes=17).cuda()
-    # network.load_state_dict(torch.load("pretrained_models/model_13_2_2_2_epoch_580.pth"))
-    network.load_state_dict(torch.load("runs/training_logs/rotdeeplabv3_87.73/checkpoints/model_22_val_loss_0.037069093.pth"))
+    # network = unet.UNet(n_channels=3, n_classes=17).cuda()
+    network = unet.RotUNet(n_channels=3, n_classes=17).cuda()
+
+    network.load_state_dict(torch.load("runs/training_logs/model_1/checkpoints/model_1_epoch_40.pth"))
 
     val_dataset = DatasetVal(val_data_path="data",
                              val_meta_path="data")
@@ -42,14 +43,6 @@ if __name__ == '__main__':
                                              batch_size=batch_size, shuffle=False,
                                              num_workers=1)
 
-    # with open("data/meta/class_weights.pkl", "rb") as file:  # (needed for python3)
-    #     class_weights = np.array(pickle.load(file))
-    # class_weights = torch.from_numpy(class_weights)
-    # class_weights = Variable(class_weights.type(torch.FloatTensor)).cuda()
-
-    # loss function
-    # loss_fn = nn.CrossEntropyLoss(weight=class_weights)
-
     network.eval()  # (set in evaluation mode, this affects BatchNorm and dropout)
     batch_losses = []
     for step, (imgs, label_imgs, img_ids) in enumerate(tqdm(val_loader)):
@@ -59,30 +52,17 @@ if __name__ == '__main__':
 
             outputs = network(imgs)  # (shape: (batch_size, num_classes, img_h, img_w))
 
-            # compute the loss:
-            # loss = loss_fn(outputs, label_imgs)
-            # loss_value = loss.data.cpu().numpy()
-            # batch_losses.append(loss_value)
 
             ########################################################################
             # save data for visualization:
             ########################################################################
-            outputs = outputs.data.cpu().numpy()  # (shape: (batch_size, num_classes, img_h, img_w))
-            pred_label_imgs = np.argmax(outputs, axis=1)  # (shape: (batch_size, img_h, img_w))
+            outputs = outputs.data.cpu().numpy()
+            pred_label_imgs = np.argmax(outputs, axis=1)
             pred_label_imgs = pred_label_imgs.astype(np.uint8)
 
             for i in range(pred_label_imgs.shape[0]):
-                pred_label_img = pred_label_imgs[i]  # (shape: (img_h, img_w))
+                pred_label_img = pred_label_imgs[i]
                 img_id = img_ids[i]
-                # img = imgs[i]  # (shape: (3, img_h, img_w))
-                #
-                # img = img.data.cpu().numpy()
-                # img = np.transpose(img, (1, 2, 0))  # (shape: (img_h, img_w, 3))
-                # img = img * np.array([0.229, 0.224, 0.225])
-                # img = img + np.array([0.485, 0.456, 0.406])
-                # img = img * 255.0
-                # img = img.astype(np.uint8)
-                #
                 pred_label_img_color = label_img_to_color(pred_label_img)
                 pred_label_img_color = pred_label_img_color[:, :, ::-1]  # BGR2RGB
                 # overlayed_img = 0.35 * img + 0.65 * pred_label_img_color
@@ -90,6 +70,3 @@ if __name__ == '__main__':
 
                 cv2.imwrite("./pred_color_imgs/" + img_id + "_color.png", pred_label_img_color)
 
-    #
-    # val_loss = np.mean(batch_losses)
-    # print("val loss: %g" % val_loss)
--- eval_on_val_for_metrics.py
+++ eval_on_val_for_metrics.py
@@ -22,33 +22,12 @@ import cv2
 
 if __name__ == '__main__':
 
-    # trainId_to_id = {
-    #     0: 0,
-    #     1: 1,
-    #     2: 2,
-    #     3: 3,
-    #     4: 4,
-    #     5: 5,
-    #     6: 6,
-    #     7: 7,
-    #     8: 8,
-    #     9: 9,
-    #     10: 10,
-    #     11: 11,
-    #     12: 12,
-    #     13: 13,
-    #     14: 14,
-    #     15: 15,
-    #     16: 16,
-    # }
-    # trainId_to_id_map_func = np.vectorize(trainId_to_id.get)
-
     batch_size = 4
     model_dir = 'runs/training_logs/model_eval_val_for_metrics'
 
     # network = unet.UNet(n_channels=3, n_classes=17).cuda()
     network = unet.RotUNet(n_channels=3, n_classes=17).cuda()
-    # print(network)
+
     network.load_state_dict(torch.load("runs/training_logs/model_1/checkpoints/model_1_epoch_40.pth"))
 
     val_dataset = DatasetVal(val_data_path="data",
@@ -61,16 +40,9 @@ if __name__ == '__main__':
                                              batch_size=batch_size, shuffle=False,
                                              num_workers=1)
 
-    # with open("data/class_weights.pkl", "rb") as file:  # (needed for python3)
-    #     class_weights = np.array(pickle.load(file))
-    # class_weights = torch.from_numpy(class_weights)
-    # class_weights = Variable(class_weights.type(torch.FloatTensor)).cuda()
-    #
-    # # loss function
-    # loss_fn = nn.CrossEntropyLoss(weight=class_weights)
+
 
     network.eval()  # (set in evaluation mode, this affects BatchNorm and dropout)
-    # batch_losses = []
     for step, (imgs, label_imgs, img_ids) in enumerate(tqdm(val_loader)):
         with torch.no_grad():  # (corresponds to setting volatile=True in all variables, this is done during inference to reduce memory consumption)
             imgs = Variable(imgs).cuda()  # (shape: (batch_size, 3, img_h, img_w))
@@ -78,31 +50,20 @@ if __name__ == '__main__':
 
             outputs = network(imgs)  # (shape: (batch_size, num_classes, img_h, img_w))
 
-            # compute the loss:
-            # loss = loss_fn(outputs, label_imgs)
-            # loss_value = loss.data.cpu().numpy()
-            # batch_losses.append(loss_value)
-
             ########################################################################
             # save data for visualization:
             ########################################################################
             outputs = F.upsample(outputs, size=(1024, 1024),
-                                 mode="bilinear")  # (shape: (batch_size, num_classes, 1024, 2048))
+                                 mode="bilinear")
 
-            outputs = outputs.data.cpu().numpy()  # (shape: (batch_size, num_classes, 1024, 2048))
-            pred_label_imgs = np.argmax(outputs, axis=1)  # (shape: (batch_size, 1024, 2048))
+            outputs = outputs.data.cpu().numpy()
+            pred_label_imgs = np.argmax(outputs, axis=1)
             pred_label_imgs = pred_label_imgs.astype(np.uint8)
 
             for i in range(pred_label_imgs.shape[0]):
-                pred_label_img = pred_label_imgs[i]  # (shape: (1024, 2048))
+                pred_label_img = pred_label_imgs[i]
                 img_id = img_ids[i]
-
-                # convert pred_label_img from trainId to id pixel values:
-                # pred_label_img = trainId_to_id_map_func(pred_label_img)  # (shape: (1024, 2048))
                 pred_label_img = pred_label_img.astype(np.uint8)
 
                 cv2.imwrite(model_dir + "/" + img_id + "_layer.png", pred_label_img)
 
-
-    # val_loss = np.mean(batch_losses)
-    # print("val loss: %g" % val_loss)
